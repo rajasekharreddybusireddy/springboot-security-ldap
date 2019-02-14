97b1cd8a66140c22c9f46055352d31f5463ac597
talk2rajasekhar <talk2rajasekhar>
2019-02-14 17:04:15 +0530

    first commit

---
+++ src/main/java/com/blogspot/javabyrajasekhar/SpringbootLdapExampleApplication.java
@@ -0,0 +1,14 @@
+package com.blogspot.javabyrajasekhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLdapExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLdapExampleApplication.class, args);
+	}
+
+}
+
